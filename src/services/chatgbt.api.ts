62c170c4e329297571ec41b73b147e9ccd9aeec1
cheangming.cheo <cheangming.cheo@sptel.com>
2025-03-26 14:41:35 +0800

    Test

--- src/services/chatgbt.api.ts
+++ src/services/chatgbt.api.ts
@@ -1,15 +1,18 @@
 "use server";
 
 export async function getChatGbtResponse(prevState: any, formData: FormData) {
-  const response = await fetch("http://localhost:8888/test", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      openaiPrompt: formData.get("openaiPrompt") as string,
-    }),
-  });
+  const response = await fetch(
+    "https://resume-backend-git-main-cheangmings-projects-44b3fb7a.vercel.app/test",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        openaiPrompt: formData.get("openaiPrompt") as string,
+      }),
+    }
+  );
 
   if (!response.ok) {
     throw new Error("Failed to fetch response from ChatGPT");
